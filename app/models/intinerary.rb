5eadfb26441bd98523ffa53cbde231607e4767d5
JMGoldsmith <spartanaudio@gmail.com>
2015-01-27 09:43:55 -0600

    fixed associations

--- app/models/intinerary.rb
+++ app/models/intinerary.rb
@@ -1,4 +1,4 @@
 class Intinerary < ActiveRecord::Base
-  has_many :user_ids, :through :user
-  has_many :event_ids, :through :event
+  has_many :user_ids, through: :user
+  has_many :event_ids, through: :event
 end
