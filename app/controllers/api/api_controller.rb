f680f71338b9054cc20ada9aa800b73369df6252
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-28 10:36:36 -0600

    create migration

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,10 +2,8 @@ module API
   class EventsController
     #will need post permissions for all data, need to see how to have ember send us the json and how we can accept it from a single source.
     #possible use of auth?
-#     def show
-#       eventList = Events.all  
-#       render json: eventList, status: 200
-#     end
+    eventList = Events.all  
+    render json: eventList, status: 200
   end
   class EventItinerariesController
     #join table
