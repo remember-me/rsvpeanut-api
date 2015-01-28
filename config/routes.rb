7a9b2ff559e0ee310d2acde7587490393237d417
JMGoldsmith <spartanaudio@gmail.com>
2015-01-28 13:49:39 -0600

    routes for events, users, eventsapi

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   # namespace :api do
-    resources :events
+    resources :events, except: :destroy
+    resources :users, except: [:destroy, :index]
+    resources :eventsapi, only: [:index, :show]
+
   # end
 
   # Example of regular route:
