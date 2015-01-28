458d612b5aef308d65cf6f42ec58d244cfdb3f14
JMGoldsmith <spartanaudio@gmail.com>
2015-01-28 12:10:02 -0600

    add route and json output for event

--- app/controllers/event_itineraries_controller.rb
+++ app/controllers/event_itineraries_controller.rb
@@ -1,2 +1,4 @@
 class EventItinerariesController < ApplicationController
+  def show
+  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
-  def show
-    eventList = Events.all  
+  def index
+    #returns all events
+    eventList = Event.all  
     render json: eventList, status: 200
   end
 end
