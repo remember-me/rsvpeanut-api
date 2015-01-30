bc4681356fb5472b80b92f34aec7728db0cb551c
JMGoldsmith <spartanaudio@gmail.com>
2015-01-30 09:28:57 -0600

    fix table name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129192132) do
+ActiveRecord::Schema.define(version: 20150130152221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,13 +43,13 @@ ActiveRecord::Schema.define(version: 20150129192132) do
     t.string   "source"
   end
 
-  create_table "intineraries", force: :cascade do |t|
+  create_table "itineraries", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "intineraries", ["user_id"], name: "index_intineraries_on_user_id", using: :btree
+  add_index "itineraries", ["user_id"], name: "index_itineraries_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "password"
@@ -61,6 +61,6 @@ ActiveRecord::Schema.define(version: 20150129192132) do
   end
 
   add_foreign_key "event_itineraries", "events"
-  add_foreign_key "event_itineraries", "intineraries"
-  add_foreign_key "intineraries", "users"
+  add_foreign_key "event_itineraries", "itineraries", column: "intinerary_id"
+  add_foreign_key "itineraries", "users"
 end
