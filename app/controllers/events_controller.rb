a8f6f84e84d718e8413056f9825f1d3a6382b92e
JMGoldsmith <spartanaudio@gmail.com>
2015-01-30 11:07:51 -0600

    added access controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,8 +10,16 @@ class EventsController < ApplicationController
     respond_to do |format|
       format.json { render :json => @newUser }
     end
+    #@newItin = EventIntinerary.new
+    #need returned event id and current user.
+    #respond_to do |format|
+      #format.json { render :json => @newItin}
+    #end
   end
   private
+  #def itin_params
+    #params.require(stuff).permit(event_id,)
+  #end
   def event_params    
     params.require(:event).permit(:name, :event_type, :location, :event_start, :event_end, :description, :lat, :long, :event_url)
   end
