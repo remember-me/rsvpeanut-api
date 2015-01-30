594e042d3a5952e6b57868b6dda72517d5bbbfb9
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-30 15:29:52 -0600

    add proper timestamp to meetup and eventbrite queries

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,8 +14,8 @@ class Event < ActiveRecord::Base
       category = e['category']['short_name'] if e['category']
       name = e['name']['text'] if e['name']
       address = e['venue']['address']['address_1'] if e['venue'] && e['venue']['address']
-      start = e['start']['local'].to_time if e['start']
-      end_time = e['end']['local'].to_time if e['end']
+      start = e['start']['local'].to_datetime if e['start']
+      end_time = e['end']['local'].to_datetime if e['end']
       description = e['description']['text'] if e['description']
       lat = e['venue']['address']['latitude'].to_f if e['venue']['address'] && e['venue']
       long = e['venue']['address']['longitude'].to_f if e['venue']['address'] && e['venue']
@@ -65,12 +65,14 @@ class Event < ActiveRecord::Base
         address = e['venue']['address_1'] if e['venue']
         lat = e['venue']['lat'].to_f if e['venue']
         lon = e ['venue']['lon'].to_f if e['venue']
-        end_time = e['time'] + e['duration'] if e['time'] && e['duration']
+        start = Time.at(e['time']).to_datetime
+        end_time = Time.at(e['time'] + e['duration']).to_datetime if e['time'] && e['duration']
+        
         {
           name: e['name'],
           event_type: params[:category],
           location: address,
-          event_start: e['time'],
+          event_start: start,
           event_end: end_time,
           attendees: e['yes_rsvp_count'],
           description: e['description'],
