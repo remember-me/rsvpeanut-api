d664492a723f867ec2bd54f68d8d67dc322a08ea
lemurgency <spenserfiller@gmail.com>
2015-02-02 15:55:35 -0600

    songkick query is prettier

--- app/models/event.rb
+++ app/models/event.rb
@@ -37,62 +37,82 @@ class Event < ActiveRecord::Base
     data
   end
 
-  def self.run_meetup_query params = { zipcode: '78701', radius: '2'}
-    event_categories = [
-      0,              'Arts',         'Business',
-      'Auto',         'Community',    'Dancing',
-      'Education',    7,              'Fashion',
-      'Fitness',      'Food & Drink', 'Games',
-      'LGBT',         'Movements',    'Well-being',
-      'Crafts',       'Languages',    'Lifestyle',
-      'Literature',   19,             'Films',
-      'Music',        'Spirituality', 'Outdoors',
-      'Paranormal',   'Moms & Dads',  'Pets',
-      'Photography',  'Beliefs',      'Sci fi',
-      'Singles',      'Social',       'Sports',
-      'Support',      'Tech',         'Women'
-      ]
-
-    url = "https://api.meetup.com/2/open_events?status=upcoming&radius=#{params[:radius]}&and_text=False&limited_events=False&desc=False&offset=0&photo-host=public&format=json&zip=#{params[:zipcode]}&page=20&sig_id=182809685&sig=1c6a45863c09b08ea6c419a14ab34c7ce2c9d17a"
-
-    if params[:category]
-      category_index = event_categories.find_index(params[:category]).to_s
-      url = "https://api.meetup.com/2/open_events?status=upcoming&radius=#{params[:radius]}&category=#{category_index}&and_text=False&limited_events=False&desc=False&offset=0&photo-host=public&format=json&zip=#{params[:zipcode]}&page=20&sig_id=182809685&sig=35aa9e882e201c5b9b672c1fad17da2376f1a208"
-    end
-
-    response = Unirest.get(url, headers: {'Accept' => 'application/json'})
-    if response.body['results']
-      data = response.body['results'].map do |e|
-        address = e['venue']['address_1'] if e['venue']
-        lat = e['venue']['lat'].to_f if e['venue']
-        lon = e ['venue']['lon'].to_f if e['venue']
-        start = Time.at(e['time']).to_datetime
-        end_time = Time.at(e['time'] + e['duration']).to_datetime if e['time'] && e['duration']
-
-        {
-          attendees: e['yes_rsvp_count'],
-          description: e['description'],
-          event_end: end_time,
-          event_start: start,
-          event_type: params[:category],
-          event_url: e['event_url'],
-          location: address,
-          lat: lat,
-          long: lon,
-          name: e['name'],
-          source: 'meetup'
-        }
-      end
-    return data
-    end
-  end
+  # def self.run_meetup_query params = { zipcode: '78701', radius: '2'}
+  #   event_categories = [
+  #     0,              'Arts',         'Business',
+  #     'Auto',         'Community',    'Dancing',
+  #     'Education',    7,              'Fashion',
+  #     'Fitness',      'Food & Drink', 'Games',
+  #     'LGBT',         'Movements',    'Well-being',
+  #     'Crafts',       'Languages',    'Lifestyle',
+  #     'Literature',   19,             'Films',
+  #     'Music',        'Spirituality', 'Outdoors',
+  #     'Paranormal',   'Moms & Dads',  'Pets',
+  #     'Photography',  'Beliefs',      'Sci fi',
+  #     'Singles',      'Social',       'Sports',
+  #     'Support',      'Tech',         'Women'
+  #     ]
+  #
+  #   base_url = "https://api.meetup.com/2/open_events?status=upcoming"
+  #   radius_url = "&radius=#{params[:radius]}"
+  #   meetup_garbage = "&and_text=False&limited_events=False&desc=False&offset=0&photo-host=public&format=json"
+  #   zipcode_url = "&zip=#{params[:zipcode]}"
+  #   meetup_key_url = "&page=20&sig_id=182809685&sig=1c6a45863c09b08ea6c419a14ab34c7ce2c9d17a"
+  #   url = base_url + radius_url + meetup_garbage + zipcode_url + meetup_key_url
+  #
+  #   if params[:category]
+  #     category_index = event_categories.find_index(params[:category]).to_s
+  #     url = "https://api.meetup.com/2/open_events?status=upcoming&radius=#{params[:radius]}&category=#{category_index}&and_text=False&limited_events=False&desc=False&offset=0&photo-host=public&format=json&zip=#{params[:zipcode]}&page=20&sig_id=182809685&sig=35aa9e882e201c5b9b672c1fad17da2376f1a208"
+  #   end
+  #
+  #   response = Unirest.get(url, headers: {'Accept' => 'application/json'})
+  #   if response.body['results']
+  #     data = response.body['results'].map do |e|
+  #       address = e['venue']['address_1'] if e['venue']
+  #       lat = e['venue']['lat'].to_f if e['venue']
+  #       lon = e ['venue']['lon'].to_f if e['venue']
+  #       start = Time.at(e['time']).to_datetime
+  #       end_time = Time.at(e['time'] + e['duration']).to_datetime if e['time'] && e['duration']
+  #
+  #       {
+  #         attendees: e['yes_rsvp_count'],
+  #         description: e['description'],
+  #         event_type: params[:category],
+  #         event_url: e['event_url'],
+  #         location: address,
+  #         lat: lat,
+  #         long: lon,
+  #         name: e['name'],
+  #         source: 'meetup'
+  #         date_start: e['start']['date'],
+  #         date_end: nil,
+  #         time_start: e['start']['time'],
+  #         time_end: nil,
+  #         utc_start: ,
+  #         utc_end: nil,
+  #         venue: e['venue']['displayName']
+  #       }
+  #     end
+  #   return data
+  #   end
+  # end
 
   def self.run_songkick_query params = { lat: 30.269560, lon: -97.742420 }
 #     this gets all of the location ids to query for events
-    url = 'http://api.songkick.com/api/3.0/events.json?location=geo:30.269560,-97.742420&apikey=xmhR3tz3sm5O55Xw'
-    response = Unirest.get(url, headers: {'Accept' => 'application/json'})
-    events = response.body['resultsPage']['results']['event']
-      .map do |event|
+    url = 'http://api.songkick.com/api/3.0/events.json'
+    lat = params[:lat].to_s
+    lon = params[:lon].to_s
+    response = Unirest.get(url,
+      headers: {
+        'Accept' => 'application/json'
+        },
+      parameters: {
+        'location' => 'geo:' + lat + ',' + lon,
+        'apikey' => 'xmhR3tz3sm5O55Xw'
+        }
+    ).body['resultsPage']['results']['event']
+
+    events = response.map do |event|
         event['start']['datetime'] ? datetime = event['start']['datetime'].to_datetime : datetime = nil
         {
           attendees: nil,
@@ -114,7 +134,6 @@ class Event < ActiveRecord::Base
         }
       end
     return events
-      # url = "http://api.songkick.com/api/3.0/metro_areas/9171/calendar.json?apikey=xmhR3tz3sm5O55Xw"
       # result = Event.run_songkick_query
       # url = "http://api.songkick.com/api/3.0/search/locations.json?location=geo:#{30.269560},#{-97.742420}&apikey=xmhR3tz3sm5O55Xw"
       # url = "http://api.songkick.com/api/3.0/metro_areas/#{ids.first}/calendar.json?apikey=xmhR3tz3sm5O55Xw"
