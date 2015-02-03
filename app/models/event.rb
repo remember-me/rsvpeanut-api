a24960075f4ab05533c7bc94de33909f221a52e1
lemurgency <spenserfiller@gmail.com>
2015-02-03 13:50:35 -0600

    eventbrite now queries multiple pages(if they are there) - no data

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,40 +2,68 @@ class Event < ActiveRecord::Base
   belongs_to :itineraries
   belongs_to :event_itineraries
 
-  def self.run_eventbrite_query params = {city: 'Austin', radius: '1mi'}
-    if params[:address]
-      url = 'https://www.eventbriteapi.com/v3/events/search/?location.address=' + params[:address] + '&location.within=' + params[:radius] + '&venue.city=' + params[:city] + '&token=DUE3OBAFNHYCQEN5E3VV'
-    else
-      url = 'https://www.eventbriteapi.com/v3/events/search/?venue.city=' + params[:city] + '&token=DUE3OBAFNHYCQEN5E3VV'
+  def self.retrieve_eventbrite_events
+    params = {
+       zipcode: '60152',
+       lat: 42.2508,
+       lon: -88.6050,
+       radius: '40',
+       page: 1
+       }
+    results = []
+    initial_response = Event.run_eventbrite_query params
+    page_count = initial_response.body['pagination']['page_count'].to_i
+    results.push Event.parse_eventbrite_data initial_response
+    if  page_count > 1
+      (2..page_count).each do |x|
+        params[:page] = x
+        results.push Event.parse_eventbrite_data Event.run_eventbrite_query params
+      end
     end
-
-    response = Unirest.get(url, headers: { "Accept" => "application/json" }, parameters: nil, auth:nil)
-    data = response.body['events'].map do |e|
-      category = e['category']['short_name'] if e['category']
-      name = e['name']['text'] if e['name']
-      address = e['venue']['address']['address_1'] if e['venue'] && e['venue']['address']
-      start = e['start']['local'].to_datetime if e['start']
-      end_time = e['end']['local'].to_datetime if e['end']
-      description = e['description']['text'] if e['description']
-      lat = e['venue']['address']['latitude'].to_f if e['venue']['address'] && e['venue']
-      long = e['venue']['address']['longitude'].to_f if e['venue']['address'] && e['venue']
-      {
-        name: name,
-        event_type: category,
-        location: address,
-        event_start: start,
-        event_end: end_time,
-#       no data for number of attendees this is for max attendees e['capacity']
-        description: description,
-        lat: lat,
-        long: long,
-        event_url: e['url'],
-        source: 'eventbrite'
-      }
-
-    end
-    data
+    return results.flatten.count
   end
+
+  def self.parse_eventbrite_data unirest_object
+        data = unirest_object.body['events'].map do |e|
+          category = e['category']['short_name'] if e['category']
+          name = e['name']['text'] if e['name']
+          address = e['venue']['address']['address_1'] if e['venue'] && e['venue']['address']
+          start = e['start']['local'].to_datetime if e['start']
+          end_time = e['end']['local'].to_datetime if e['end']
+          description = e['description']['text'] if e['description']
+          lat = e['venue']['address']['latitude'].to_f if e['venue']['address'] && e['venue']
+          long = e['venue']['address']['longitude'].to_f if e['venue']['address'] && e['venue']
+          {
+    #         name: name,
+    #         event_type: category,
+    #         location: address,
+    #         event_start: start,
+    #         event_end: end_time,
+    # #       no data for number of attendees this is for max attendees e['capacity']
+    #         description: description,
+    #         lat: lat,
+    #         long: long,
+    #         event_url: e['url'],
+            source: 'eventbrite'
+          }
+
+        end
+        data
+  end
+
+  def self.run_eventbrite_query params
+    url = 'https://www.eventbriteapi.com/v3/events/search/'
+    eventbrite_token ='DUE3OBAFNHYCQEN5E3VV'
+    response = Unirest.get(url, headers: { "Accept" => "application/json" },
+      parameters: {
+        'location.latitude' => params[:lat].to_s,
+        'location.longitude' => params[:lon].to_s,
+        'location.within' => params[:radius] + 'mi',
+        'token' => eventbrite_token,
+        'page' => params[:page]
+        })
+  end
+
   def self.retrieve_all_meetup_categories
     url = "https://api.meetup.com/2/categories"
     event_categories = Unirest.get(url,
@@ -47,8 +75,6 @@ class Event < ActiveRecord::Base
         'offset' => '0',
         'photo-host' => 'public',
         'format' => 'json',
-        # 'sig' => 'bd0e7c969aba74156e487839a550dd155cb8a9b0',
-        # 'sig_id' => '134482232',
         'page' => '40'
         }).body['results']
     results = event_categories.map do |cat|
