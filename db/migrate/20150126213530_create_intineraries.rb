80aeebf6d5d5cfc9f9967e104b5054dcd599d7d7
JMGoldsmith <spartanaudio@gmail.com>
2015-02-03 09:15:38 -0600

    spell check

--- db/migrate/20150126213530_create_intineraries.rb
+++ db/migrate/20150126213530_create_intineraries.rb
@@ -1,12 +1,12 @@
-class CreateIntineraries < ActiveRecord::Migration
+class CreateItineraries < ActiveRecord::Migration
   def change
-    create_table :intineraries do |t|
+    create_table :itineraries do |t|
       t.references :user, index: true
       t.references :event, index: true
 
       t.timestamps null: false
     end
-    add_foreign_key :intineraries, :users
-    add_foreign_key :intineraries, :events
+    add_foreign_key :itineraries, :users
+    add_foreign_key :itineraries, :events
   end
 end
