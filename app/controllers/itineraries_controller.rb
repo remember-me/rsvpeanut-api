2e14c704ff76b693b42433db50a4ec403095d607
JMGoldsmith <spartanaudio@gmail.com>
2015-02-05 12:14:09 -0600

    can add itin and events

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,35 +1,43 @@
+require 'byebug'
 class ItinerariesController < ApplicationController
   skip_before_filter  :verify_authenticity_token
+
   def index
     @itineraryList = Itinerary.all
     render json: @itineraryList, status: 200
   end
+
   def show
     @showItin = Itinerary.find(itin_params)
     render json: @showItin, status: 200
   end
+
   def create
-    @newItinerary = Itinerary.new itin_params#change to what is in :id
+    @newItinerary = Itinerary.new itin_params
     @newItinerary.save
-    respond_to do |format|
-      format.json { render :json =>{ :itineraries => @newItinerary}}
+
+    @eventArray = event_array#unpermitted parameters
+    @eventArray.each do |event|
+      event = Event.new(event)  #iterate through
+
+      event.save
     end
-    @newEvent = Event.new event_params #iterate through
-    @newEvent.save
-    respond_to do |format|
-      format.json { render :json => {:event => @newEvent} }
-    end
-    @newEventItin = EventItinerary.new(params[:id]) #pull each event_id and itinerary_id.
+
+    @newEventItin = EventItinerary.new(params[:id])#is not pulling in the returned itin ID or event ID.
     @newEventItin.save
+    
     respond_to do |format|
-      format.json { render :json =>{:event_itineraries => @newEventItin}}
+      format.json { render :json => [{:event => @newEvent},{ :itineraries => @newItinerary},{:event_itineraries => @newEventItin} ]}
     end
   end 
   private
   def event_params    
-    params.require(:event).permit(:name, :event_type, :location, :utc_start, :utc_end, :description, :lat, :long, :event_url,:attendees,:cost,:source,:date_start,:date_end,:time_start,:time_end,:venue)
+    params.permit(:name, :event_type, :location, :utc_start, :utc_end, :description, :lat, :long, :event_url,:attendees,:cost,:source,:date_start,:date_end,:time_start,:time_end,:venue)
   end
   def itin_params
     params.permit(:user_id)#need to change to whats in params.
+  end
+  def event_array
+    params.permit(event: [:name, :event_type, :location, :utc_start, :utc_end, :description, :lat, :long, :event_url,:attendees,:cost,:source,:date_start,:date_end,:time_start,:time_end,:venue]).require(:event)
   end
 end
