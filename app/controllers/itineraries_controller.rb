147482540be48764099c9fa95e5ccf2656652b69
JMGoldsmith <spartanaudio@gmail.com>
2015-02-05 10:18:50 -0600

    params uploading

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -9,24 +9,27 @@ class ItinerariesController < ApplicationController
     render json: @showItin, status: 200
   end
   def create
-    @newItinerary = Itinerary.new itin_params
+    @newItinerary = Itinerary.new itin_params#change to what is in :id
     @newItinerary.save
     respond_to do |format|
       format.json { render :json =>{ :itineraries => @newItinerary}}
     end
-    @newEvent = Event.new event_params
+    @newEvent = Event.new event_params #iterate through
     @newEvent.save
     respond_to do |format|
       format.json { render :json => {:event => @newEvent} }
     end
-    @newEventItin = EventItinerary.new itin_params
+    @newEventItin = EventItinerary.new(params[:id]) #pull each event_id and itinerary_id.
     @newEventItin.save
     respond_to do |format|
       format.json { render :json =>{:event_itineraries => @newEventItin}}
     end
   end 
   private
+  def event_params    
+    params.require(:event).permit(:name, :event_type, :location, :utc_start, :utc_end, :description, :lat, :long, :event_url,:attendees,:cost,:source,:date_start,:date_end,:time_start,:time_end,:venue)
+  end
   def itin_params
-    params.require(:itineraries).permit(:user_id)
+    params.permit(:user_id)#need to change to whats in params.
   end
 end
